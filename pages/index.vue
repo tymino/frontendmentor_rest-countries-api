872059eb9ff320c5d123ca76bed352068def3125
Aleksey <d_team-lex@bk.ru>
2022-11-12 15:37:40 +0300

    test fetch

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
-<script setup>
-console.log('main page');
-</script>
-
 <template>
   <div class="main">
-    <h1>Welcome to the homepage</h1>
+    <h1 v-for="(c, index) in countriesForPage" :key="index">
+      Welcome to the homepage
+    </h1>
   </div>
 </template>
+
+<script setup>
+const { data: countries } = await useFetch(
+  'https://restcountries.com/v3.1/all'
+);
+const countriesForPage = await countries.value.splice(0, 10);
+
+console.log('main page', countriesForPage);
+</script>
 
 <style lang="scss" scoped></style>
