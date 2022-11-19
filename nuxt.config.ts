4a123093af9f47e6ca5922d785d49bf110123b17
Aleksey <d_team-lex@bk.ru>
2022-11-19 16:50:09 +0300

    add favicon.png

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1.0',
       title: 'Frontendmentor | Rest countries API',
+      link: [{ rel: 'icon', type: 'image/png', href: '/favicon-32x32.png' }],
     },
   },
   css: ['@/layouts/global.css'],
