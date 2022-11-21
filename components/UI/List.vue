e48b04332a62d6437421b7dc22451411b3a0d536
Aleksey <d_team-lex@bk.ru>
2022-11-21 15:26:09 +0300

    add template list component

---
+++ components/UI/List.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="list">
+    <div class="list__line" v-for="data in listData" :key="data">
+      <span>{{ data.name }}</span>
+      {{ data.payload }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    listData: {
+      type: Array,
+      default: () => [],
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
